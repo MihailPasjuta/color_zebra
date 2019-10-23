7c247ed6d1fe4ab7320f343852ec4f4d75edd19a
mihail <mihailpaslevel@gmail.com>
2019-10-23 14:31:02 +0300

    commit

--- scripts/menu_burger.js
+++
@@ -1,9 +0,0 @@
-export default function myFunction() {
-			var x = document.getElementById("myTopnav");
-			if (x.className === "topnav") {
-				x.className += " responsive";
-			} 
-			else {
-				x.className = "topnav";
-			}
-		}
